eddff19885a4508882123c5ef5e2f6686e62173c
onigaaris <onigaaris50@gmail.com>
2025-05-06 14:58:52 +0300

    Initial commit

---
+++ src/main/java/com/youngedremastered/youngedremastered/YoungedremasteredApplication.java
@@ -0,0 +1,13 @@
+package com.youngedremastered.youngedremastered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungedremasteredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoungedremasteredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youngedremastered/youngedremastered/YoungedremasteredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngedremastered.youngedremastered;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoungedremasteredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
